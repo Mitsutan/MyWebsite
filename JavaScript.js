d1a0476a966f9be57f7926ca3a7a1ede37156b48
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2021-05-20 23:57:02 +0900

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,6 +0,0 @@
-﻿$(function () {
-    $(window).scroll(function () {
-        var y = $(this).scrollTop();
-        $('#body').css('background-position', '0 ' + parseInt(-y / 5) + 'px');
-    });
-});
