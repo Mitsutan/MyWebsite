df2c90caff518ec44e3147bed4a02f320a497f7c
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2021-05-20 23:57:33 +0900

    Add files via upload

---
+++ javascript/JavaScript.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        $('#body').css('background-position', '0 ' + parseInt(-y / 5) + 'px');
+
+        if ($(this).scrollTop() > $(this).height()) {
+            $("#scroll_to_top").fadeIn();
+        } else {
+            $("#scroll_to_top").fadeOut();
+        };
+    });
+
+    // 「TOPに戻る」ボタンがクリックされた時の動きを指定
+    $("#scroll_to_top").click(function () {
+        // ページの一番上までスクロール
+        $('body, html').animate({ scrollTop: 0 }, 300, 'linear');
+    })
+
+
+});
+
+
