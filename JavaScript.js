00f8fa073b8b2ef3376ad24d7cef52fbe1589720
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2021-05-20 16:46:21 +0900

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        $('#body').css('background-position', '0 ' + parseInt(-y / 5) + 'px');
+    });
+});
